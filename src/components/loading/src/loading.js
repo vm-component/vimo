6990afec3090a597878cab8fe0d91fc466870ef7
向松涛 <280304286@163.com>
2016-12-27 16:36:04 +0800

    loading组件

--- src/components/loading/src/loading.js
+++ src/components/loading/src/loading.js
@@ -4,25 +4,86 @@
 import Vue from 'vue';
 const loading = require('./loading.vue');
 const loadingConstructor = Vue.extend(loading);
+// 自动关闭后的callback注册,loading的话只会有一个是当期打开的
+let _onDidDismissCallBacks = [];
+// 当前开启的toast序号
+let _loadingIndex = -1;
 
-
-// 实例方法挂载
 
 /**
  * Present(呈现) the instance.
  * @returns {Promise} Returns a promise which is resolved when the transition has completed.
  */
 loadingConstructor.prototype.present = function () {
+	const _this = this;
+	// 一个应用当前只能打开一个
+	// console.log(_loadingIndex)
+	// if(!!document.getElementById('ion-loading')){
+	// 	alert('一次只能开启一个')
+	// 	return false
+	// }
+
+	if (!!_this.duration && typeof _this.duration === 'number' && _this.duration > 0) {
+		// 如果有持续时间->时间到期后如果注册了onDidDismiss回调的话
+		// 保存当前的序号
+		let _index = _this.loadingIndex;
+		setTimeout(function () {
+			_this.dismiss(parseInt(_index))
+		}, _this.duration);
+	}
+
 	// this为组件实例，调用组件自己的方法。
-	return this.present();
+	return _this._present();
+
 };
+
 
 /**
  * Dismiss(撤销) instance.
  * @returns {Promise} Returns a promise which is resolved when the transition has completed.
  */
-loadingConstructor.prototype.dismiss = function () {
-	return this.dismiss();
+loadingConstructor.prototype.dismiss = function (index) {
+	const _this = this;
+	return _this._dismiss().then(function () {
+		if (index != null) {
+			// 执行指定的index
+			_doThisToastIndex(parseInt(index))
+		} else {
+			// 否则执行当前环境的index
+			_doThisToastIndex(parseInt(_this.loadingIndex))
+		}
+	});
+};
+
+/**
+ * 执行输入位置的_onDidDismissCallBacks内部callback
+ * @param {Number} index -  _onDidDismissCallBacks中待执行的的序列号，执行完毕后更新数组
+ * */
+function _doThisToastIndex(index) {
+	for (let i = 0, _len = _onDidDismissCallBacks.length; _len > i; i++) {
+		let _tmp = _onDidDismissCallBacks[i];
+		if (parseInt(_tmp.i) === parseInt(index)) {
+			_tmp.cb();
+			_onDidDismissCallBacks.splice(i, 1);
+			break;
+		}
+	}
+}
+
+
+/**
+ * onDidDismiss(撤销) the callback.
+ * 注册自动关闭后的回调
+ */
+loadingConstructor.prototype.onDidDismiss = function (callback) {
+	const _this = this;
+	if (!!callback && typeof callback === 'function') {
+		// 推入缓存数组
+		_onDidDismissCallBacks.push({
+			i: _this.loadingIndex,
+			cb: callback,
+		});
+	}
 };
 
 
@@ -36,11 +97,14 @@ let getAnInstance = () => {
 };
 
 /**
- * 使用create创建ActionSheet的实例，与IONIC保持一致
+ * 使用create创建实例，与IONIC保持一致
  * */
-let ActionSheet = {
+let Loading = {
 	create: function (options = {}) {
+		_loadingIndex++;
 		let instance = getAnInstance();
+		// 标记当前创建的toastID
+		instance.loadingIndex = _loadingIndex;
 		// 参数传入
 		instance.spinner = !!options.spinner ? options.spinner.trim() : '';
 		instance.content = !!options.content ? options.content.trim() : '';
@@ -51,10 +115,11 @@ let ActionSheet = {
 
 		// 插入DOM中
 		document.body.appendChild(instance.$el);
+
 		return instance;
 	}
 };
 
-export default ActionSheet;
+export default Loading;
 
 
--- src/components/loading/src/loading.vue
+++ src/components/loading/src/loading.vue
@@ -1,33 +1,141 @@
 <template>
-    <div class="ion-loading">
-        <ion-backdrop v-show="showBackdrop"></ion-backdrop>
-        <div class="loading-wrapper">
-            <div v-if="showSpinner" class="loading-spinner">
-                <ion-spinner :name="spinner"></ion-spinner>
+    <div id="ion-loading" class="ion-loading" :class="[modeClass,cssClass]">
+        <ion-backdrop :isActive="isActive" v-show="showBackdrop" :enableBackdropDismiss="false"></ion-backdrop>
+        <transition name="loading"
+                    v-on:before-enter="_beforeEnter"
+                    v-on:after-enter="_afterEnter"
+                    v-on:before-leave="_beforeLeave"
+                    v-on:after-leave="_afterLeave">
+            <div class="loading-wrapper" v-show="isActive">
+                <div v-if="showSpinner" class="loading-spinner">
+                    <ion-spinner :name="spinner"></ion-spinner>
+                </div>
+                <div v-if="content" v-html="content" class="loading-content"></div>
             </div>
-            <div v-if="d.content" v-html="content" class="loading-content"></div>
-        </div>
+        </transition>
     </div>
 </template>
 <style scoped lang="scss">
     @import './loading';
     @import './loading.ios';
-    @import './loading.md';
-    @import './loading.wp';
+    /*@import './loading.md';*/
+    /*@import './loading.wp';*/
 
+    .loading-enter-active, .loading-leave-active {
+        transform: scale(1);
+        opacity: 1;
+    }
+
+    .loading-enter {
+        transform: scale(1.1);
+        opacity: 0;
+    }
+
+    .loading-leave-active {
+        transform: scale(0.9);
+        opacity: 0;
+    }
 
 </style>
 <script type="text/ecmascript-6">
     import Spinner from '../../spinner';
-
+    import Backdrop from '../../backdrop';
 
     export default{
         data(){
-            return {}
+            return {
+                /**
+                 * 初始化Instance的数据
+                 * 因为是实例调用模式，故prop和data在初始化后是同样的数据接口，
+                 * 故prop就没有存在的价值
+                 * */
+                spinner: 'ios', // String
+                content: null, // 可以使html片段
+                cssClass: null,
+                showBackdrop: false,
+                duration: null, // 自动关闭时间
+                // TODO:这部分没做。
+                dismissOnPageChange: false,// 页面切花是否关闭
+
+                /**
+                 * 组件状态
+                 * */
+                isActive: false, // 开启状态
+                enabled: false, // 是否在过渡态的状态判断，如果在动画中则为false
+                // TODO: mode需要从配置中取,最好和scss同步
+                mode: 'ios', // ios?android?windown?we?alipay
+
+            }
         },
         watch: {},
-        computed: {},
-        methods: {},
+        computed: {
+            // 设置ActionSheet的风格
+            modeClass: function () {
+                return `loading-${this.mode}`
+            },
+            showSpinner: function () {
+                return this.spinner !== 'hide'
+            },
+
+        },
+        methods: {
+            /**
+             * ActionSheet Animate Hooks
+             * */
+            _beforeEnter: function () {
+                this.enabled = false; // 不允许过渡中途操作
+            },
+            _afterEnter: function () {
+                this.enabled = true;
+            },
+            _beforeLeave: function () {
+                this.enabled = false;
+            },
+            _afterLeave: function () {
+                this.enabled = true;
+                // 删除DOM
+                this.$el.remove()
+            },
+
+
+            /**
+             * Present the action sheet instance.
+             * 这个是内部函数，外部同名方法会处理额外事宜
+             * @returns {Promise} Returns a promise which is resolved when the transition has completed.
+             */
+            _present: function () {
+                const _this = this;
+                _this.isActive = true;
+                return new Promise(function (resolve) {
+                    let _presentHandler = function () {
+                        _this.$el.removeEventListener('transitionend', _presentHandler);
+                        resolve('Present Success!');
+                    };
+                    _this.$el.addEventListener('transitionend', _presentHandler);
+                });
+            },
+
+            /**
+             * Dismiss the instance.
+             * @returns {Promise} Returns a promise which is resolved when the transition has completed.
+             */
+            _dismiss: function () {
+                const _this = this;
+                if (!_this.enabled) {
+                    return false
+                }
+                _this.enabled = false;
+                _this.isActive = false; // 动起来
+                return new Promise(function (resolve) {
+                    let _dismissHandler = function () {
+                        _this.$el.removeEventListener('transitionend', _dismissHandler);
+                        _this.enabled = true;
+                        resolve('Dismiss Success!');
+                    };
+                    _this.$el.addEventListener('transitionend', _dismissHandler);
+                });
+            },
+        },
         created: function () {
         },
         mounted: function () {
@@ -35,7 +143,8 @@
         activated: function () {
         },
         components: {
-        	'ion-spinner':Spinner
+            'ion-backdrop': Backdrop,
+            'ion-spinner': Spinner
         }
     }
 </script>
