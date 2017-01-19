c3de1f07b0cc4e9594522037c07d0d631cc7acae
向松涛 <280304286@163.com>
2017-01-19 23:32:01 +0800

    badge组件

---
+++ src/components/badge/src/badage.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="ion-badge" :class="[modeClass,colorClass]">
+        <slot></slot>
+    </div>
+</template>
+<style lang="scss">
+    @import "badge";
+    @import "badge.ios";
+</style>
+<script type="text/ecmascript-6">
+    export default{
+        data(){
+            return{}
+        },
+        props:{
+            /**
+             * 按钮color：primary、secondary、danger、light、dark
+             * */
+            color: {
+                type: String,
+                default: '',
+            },
+            /**
+             * mode 按钮风格 ios/window/android/we/alipay
+             * */
+            mode: {
+                type: String,
+                default: 'ios',
+            },
+        },
+        computed:{
+            // 环境样式
+            modeClass: function () {
+                return `badge badge-${this.mode}`
+            },
+            // 颜色
+            colorClass: function () {
+                return !!this.color ? (`badge-${this.mode}-${this.color}`) : ''
+            },
+        },
+        methods:{},
+        created:function(){},
+        mounted:function(){},
+        activated:function(){},
+        components:{}
+    }
+</script>
