84d84e246b9df2f925b524adf3b9ef421eb98a32
向松涛 <280304286@163.com>
2017-01-19 23:31:22 +0800

    thumbnail组件

---
+++ src/components/thumbnail/src/thumbnail.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="ion-thumbnail">
+        <slot></slot>
+    </div>
+</template>
+<style lang="scss">
+
+</style>
+<script type="text/ecmascript-6">
+    export default{
+        data(){
+            return{}
+        },
+        watch:{},
+        computed:{},
+        methods:{},
+        created:function(){},
+        mounted:function(){},
+        activated:function(){},
+        components:{}
+    }
+</script>
