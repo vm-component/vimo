75e208e292bd792c8a38c6cb0968e9e147971285
向松涛 <280304286@163.com>
2017-01-19 23:32:18 +0800

    card组件

---
+++ src/components/card/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Hsiang on 2017/1/17.
+ */
+import card from './src/card.vue';
+import cardContent from './src/card-content.vue';
+import cardHeader from './src/card-header.vue';
+import cardTitle from './src/card-title.vue';
+module.exports = {
+	Card:card,
+	CardContent:cardContent,
+	CardHeader:cardHeader,
+	CardTitle:cardTitle,
+};
---
+++ src/components/card/src/card.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="ion-card" :class="[modeClass,colorClass]">
+        <slot></slot>
+    </div>
+</template>
+<style lang="scss">
+    @import "card";
+    @import "card.ios";
+</style>
+<script type="text/ecmascript-6">
+    export default{
+        data(){
+            return{
+                componentName:'card'
+            }
+        },
+        props:{
+            /**
+             * 按钮color：primary、secondary、danger、light、dark
+             * */
+            color: {
+                type: String,
+                default: '',
+            },
+            /**
+             * mode 按钮风格 ios/window/android/we/alipay
+             * */
+            mode: {
+                type: String,
+                default: 'ios',
+            },
+        },
+        watch:{},
+        computed:{
+            // 环境样式
+            modeClass: function () {
+                return `${this.componentName} ${this.componentName}-${this.mode}`
+            },
+            // 颜色
+            colorClass: function () {
+                return !!this.color ? (`${this.componentName}-${this.mode}-${this.color}`) : ''
+            },
+        },
+        methods:{},
+        created:function(){},
+        mounted:function(){},
+        activated:function(){},
+        components:{}
+    }
+</script>
