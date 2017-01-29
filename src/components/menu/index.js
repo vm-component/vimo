cc53ce188b1d3df5c4fc1d2d5350f31e7d14249e
向松涛 <280304286@163.com>
2017-01-29 20:27:06 +0800

    menu组件

---
+++ src/components/menu/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Hsiang on 2017/1/29.
+ */
+import Menu from './src/menu.vue';
+module.exports = Menu;
---
+++ src/components/menu/src/menu.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="ion-menu">
+    <div class="menu-inner">
+      <slot></slot>
+    </div>
+    <ion-backdrop></ion-backdrop>
+  </div>
+</template>
+<style lang="scss">
+  @import './menu';
+  @import './menu.ios';
+
+</style>
+<script type="text/ecmascript-6">
+  export default{
+    name: 'ion-menu',
+    data(){
+      return {}
+    },
+    props: {},
+    watch: {},
+    computed: {},
+    methods: {},
+    created(){},
+    mounted(){},
+    activated(){},
+    components: {}
+  }
+</script>
