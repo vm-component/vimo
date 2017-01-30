f02480bad15c97256832561c2a0b3ee151aee2ce
向松涛 <280304286@163.com>
2017-01-30 22:28:36 +0800

    backdrop全局注册

--- src/components/backdrop/src/backdrop.vue
+++ src/components/backdrop/src/backdrop.vue
@@ -1,90 +1,91 @@
 <template>
-    <transition
-            name="fade"
-            v-on:before-enter="_beforeEnter"
-            v-on:after-leave="_afterLeave">
-        <div class="ion-backdrop" :class="{'backdrop-no-tappable':!enableBackdropDismiss}" v-show="isActive"></div>
-    </transition>
+  <transition
+    name="fade"
+    v-on:before-enter="_beforeEnter"
+    v-on:after-leave="_afterLeave">
+    <div class="ion-backdrop" :class="{'backdrop-no-tappable':!enableBackdropDismiss}" v-show="isActive"></div>
+  </transition>
 </template>
 <script type="text/ecmascript-6">
 
-    export default{
-        props: {
-            enableBackdropDismiss: {
-                type: Boolean,
-                default: true
-            },
-            //  isActive
-            isActive: {
-                type: Boolean,
-                default: false
-            },
-        },
-        methods: {
-            // private
-            // 过渡钩子
-            _beforeEnter: function (el) {
-                this.$emit('$backdropShown');
-            },
-            _afterLeave: function (el) {
-                this.$emit('$backdropHidden');
-            },
+  export default{
+    name: 'ion-backdrop',
+    props: {
+      enableBackdropDismiss: {
+        type: Boolean,
+        default: true
+      },
+      //  isActive
+      isActive: {
+        type: Boolean,
+        default: false
+      },
+    },
+    methods: {
+      // private
+      // 过渡钩子
+      _beforeEnter: function (el) {
+        this.$emit('$backdropShown');
+      },
+      _afterLeave: function (el) {
+        this.$emit('$backdropHidden');
+      },
 
-            //public
-            // 开启
-            retain: function () {
-                this.isActive = true;
-            },
-            // 关闭
-            release: function () {
-                if (this.enableBackdropDismiss) {
-                    this.isActive = false;
-                }
-            },
-        },
-        mounted: function () {
-            // this.$on('$backdropShown', function () {
-            //     console.log('--beforeEnter')
-            // });
-            // this.$on('$backdropHidden', function () {
-            //     console.log('--afterLeave')
-            // })
+      //public
+      // 开启
+      retain: function () {
+        this.isActive = true;
+      },
+      // 关闭
+      release: function () {
+        if (this.enableBackdropDismiss) {
+          this.isActive = false;
         }
-
+      },
+    },
+    mounted: function () {
+      // this.$on('$backdropShown', function () {
+      //     console.log('--beforeEnter')
+      // });
+      // this.$on('$backdropHidden', function () {
+      //     console.log('--afterLeave')
+      // })
     }
+
+  }
 </script>
 <style scoped lang="scss">
-    // Backdrop
-    // --------------------------------------------------
-    $backdrop-color: #000 !default;
-    $z-index-backdrop: 10;
+  // Backdrop
+  // --------------------------------------------------
+  $backdrop-color: #000 !default;
+  $z-index-backdrop: 10;
 
-    .ion-backdrop {
-        position: absolute;
-        top: 0;
-        left: 0;
-        z-index: $z-index-backdrop;
-        display: block;
+  .ion-backdrop {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: $z-index-backdrop;
+    display: block;
 
-        width: 100%;
-        height: 100%;
+    width: 100%;
+    height: 100%;
 
-        background-color: $backdrop-color;
-        opacity: .4;
-        transform: translateZ(0);
-    }
+    background-color: $backdrop-color;
+    opacity: .4;
+    transform: translateZ(0);
+  }
 
-    .ion-backdrop.backdrop-no-tappable {
-        cursor: auto;
-    }
+  .ion-backdrop.backdrop-no-tappable {
+    cursor: auto;
+  }
 
-    /*fade animate class*/
-    .fade-enter-active, .fade-leave-active {
-        transition: opacity 400ms;
-    }
+  /*fade animate class*/
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 400ms;
+  }
 
-    .fade-enter, .fade-leave-active {
-        opacity: 0
-    }
+  .fade-enter, .fade-leave-active {
+    opacity: 0
+  }
 
 </style>
