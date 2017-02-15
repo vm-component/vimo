f4e25b9f82a62a71409b94fd718a31565c6bade8
向松涛 <280304286@163.com>
2017-02-15 09:51:59 +0800

    title无法修改微信title的bug修复

--- src/components/toolbar/src/toolbar-title.vue
+++ src/components/toolbar/src/toolbar-title.vue
@@ -78,19 +78,23 @@
         let iframe;
         const _this = this;
         if (!!val && val != document.title) {
-          // 利用iframe的onload事件刷新页面
-          document.title = val;
-          iframe = document.createElement('iframe');
-          iframe.src = ' '; // iframe.src = '/static/favicon.ico';
-          iframe.style.visibility = 'hidden';
-          iframe.style.width = '1px';
-          iframe.style.height = '1px';
-          iframe.onload = function () {
-            _this.$nextTick(function () {
-              document.body.removeChild(iframe);
-            });
-          };
-          document.body.appendChild(iframe);
+          //以下代码可以解决以上问题，不依赖jq
+          setTimeout(function () {
+            //利用iframe的onload事件刷新页面
+            document.title = val;
+            let iframe = document.createElement('iframe');
+            // iframe.src = '/static/favicon.ico';
+            iframe.src = 'favicon.ico';
+            iframe.style.visibility = 'hidden';
+            iframe.style.width = '1px';
+            iframe.style.height = '1px';
+            iframe.onload = function () {
+              setTimeout(function () {
+                document.body.removeChild(iframe);
+              }, 0);
+            };
+            document.body.appendChild(iframe);
+          }, 0);
 
           // 修改ion-header的title
           _this.titleInner = val;
