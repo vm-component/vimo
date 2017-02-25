a2155ae84c107dd26f522a1130c86c060e9ad6e9
向松涛 <280304286@163.com>
2017-02-25 14:14:43 +0800

    icon转向font-awesome字体

--- src/vimo/components/icon/icon.vue
+++ src/vimo/components/icon/icon.vue
@@ -1,21 +1,23 @@
 <template>
   <i class="icon ion-icon"
-     :class="[modeClass,colorClass,nameClass,iosClass,mdClass,itemClass]"></i>
+     :class="[colorClass,nameClass,itemClass]"></i>
 </template>
 <style lang="scss">
   @import "icon";
-  @import "icon.ios";
-  /*@import "icon.md";*/
-  /*@import "icon.wp";*/
-  @import "scss/ionicons";
-
+  @import "./font-awesome/css/font-awesome.css";
 </style>
 <script type="text/ecmascript-6">
   export default{
     name: 'Icon',
     props: {
+
       /**
-       * 按钮color：primary、ghost、dashed、text、info、success、warning、error
+       * 按钮color：
+       * primary:    #007aff,
+       * secondary:  #32db64,
+       * danger:     #d91e18,
+       * light:      #f4f4f4,
+       * dark:       #222,
        * */
       color: {
         type: String,
@@ -23,16 +25,7 @@
       },
 
       /**
-       * The mode to apply to this component.
-       * mode 按钮风格 ios/window/android/we/alipay
-       * */
-      mode: {
-        type: String,
-        default:  VM.config.get('mode') || 'ios',
-      },
-
-      /**
-       * Icon to use. Will load the appropriate icon for each mode
+       * icon的名字, 默认的名字
        * */
       name: {
         type: String,
@@ -40,76 +33,108 @@
       },
 
       /**
-       * Explicitly set the icon to use on ios
+       * 激活状态的图标样式,
+       * 如果未定义,则为:`${this.name}-o`
        * */
-      ios: {
+      activeName: {
         type: String,
         default: '',
       },
 
       /**
-       * Explicitly set the icon to use on MD
-       * */
-      md: {
-        type: String,
-        default: '',
-      },
-
-      /**
-       * Whether or not the icon has an "active" appearance.
-       * On iOS an active icon is filled in or full appearance,
-       * and an inactive icon on iOS will use an outlined version
-       * of the icon same icon. Material Design icons do not
-       * change appearance depending if they're active or not.
-       * The isActive property is largely used by the tabbar.
+       * 表示当前的图标的状态
+       * 这里使用fontawesome图标, 意味着, 当isActive为true时,
+       * 需要给name的名字后面增加'-o'的字段, 表示为激活状态.
+       *
+       * 特殊情况:
+       * 当定义了activeIcon时, 则使用这个字段的值作为激活状态的图标
+       *
+       * 在name中设置normal状态的图标
+       * 在activeName中设置激活状态的图标样式
+       *
        * */
       isActive: {
         type: Boolean,
         default: false,
       },
-      // role:{
-      //   type: String,
-      //   default: null,
-      // }
+
     },
     data(){
       return {
+        nameClass: '', // 最终显示的nameClass
         itemClass: '',
+
+        nameValue: '', // 过滤后的值
+        activeNameValue: '', // 过滤后的值
       }
     },
+    watch: {
+      isActive () {
+        this.init();
+      },
+    },
     computed: {
-      // 环境样式
-      modeClass: function () {
-        return !!this.mode ? `icon-${this.mode}` : '';
-      },
       // 颜色
-      colorClass: function () {
-        return !!this.color ? (`icon-${this.mode}-${this.color}`) : ''
+      colorClass () {
+        return !!this.color ? (`icon-${this.color}`) : ''
       },
-      nameClass: function () {
-        let val = this.name;
-        if (!!this.name && !(/^md-|^ios-|^logo-/.test(val))) {
-          // if (!!this.name && !(/^md-|^ios-|^logo-/.test(val))) {
-          // this does not have one of the defaults
-          // so lets auto add in the mode prefix for them
-          return 'ion-' + this.mode + '-' + val;
-        } else if (!!this.name && (/^logo-/.test(val))) {
-          return 'ion-' + val;
+    },
+    methods: {
+      /**
+       * name的过滤规则
+       * */
+      getFilteredName(name){
+        name = name.trim();
+
+        if (name.indexOf('fa-') === 0) {
+          return `fa ${name}`;
+        }
+
+        // 如果不是fa, 则直接将name当做class
+        return name
+      },
+
+      /**
+       * activeName的过滤规则
+       * */
+      getFilteredActiveName(){
+        let name = this.activeName;
+        if (!!name) {
+          return this.getFilteredName(name)
         } else {
-          return val;
+          if (this.name.indexOf('fa-') === 0) {
+            // font-awesome 的激活状态是后面带'-o'
+            return `fa ${this.name}` + '-o';
+          } else {
+            return ''
+          }
         }
       },
-      iosClass: function () {
-        return !!this.ios ? ('ion-ios' + '-' + this.ios) : '';
+
+      /**
+       * 初始化
+       * */
+      init(){
+        /**
+         * 过滤后的name
+         * */
+        this.nameValue = this.getFilteredName(this.name);
+        this.activeNameValue = this.getFilteredActiveName();
+
+        /**
+         * 是否为激活状态
+         */
+        if (this.isActive) {
+          this.nameClass = this.activeNameValue;
+        } else {
+          this.nameClass = this.nameValue;
+        }
       },
-      mdClass: function () {
-        return !!this.md ? ('ion-md' + '-' + this.md) : '';
-      },
-      // roleClass:function () {
-      //   return !!this.role ? (`${this.role}-icon`) : '';
-      // }
     },
-    mounted: function () {
+    created(){
+      this.init();
+    },
+    mounted(){
 
       if (!!this.$parent.$el && this.$parent.$el.className && this.$parent.$el.className.indexOf('item') > -1) {
         //	button in items should add class of 'item-button'
