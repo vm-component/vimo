c4e977c39570ecacfb8e1d91f6f4779abaf4c1e0
向松涛 <280304286@163.com>
2017-03-02 10:54:07 +0800

    add geolocation module

---
+++ src/vimo/geolocation/geolocation-registry.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Hsiang on 2017/3/2.
+ * geolocatation配置信息
+ */
+export default GEOLOCATION_CONFIG = [
+  {
+    type: 'qq',
+    app_key: 'yourkey',
+    app_name: 'yourappname'
+  },
+  {
+    type: 'baidu',
+    app_key: 'yourkey',
+    app_name: 'yourappname'
+  },
+  {
+    type: 'ali',
+    app_key: 'yourkey',
+    app_name: 'yourappname'
+  }
+];
