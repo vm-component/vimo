9fc292904f5d0d8a7d3e30a20a7e713fea717071
向松涛 <280304286@163.com>
2017-03-14 16:10:24 +0800

    无限滚动组件编写

---
+++ src/vimo/components/infinite-scroll/index.js
@@ -0,0 +1,7 @@
+import InfiniteScroll from './infinite-scroll.vue';
+import InfiniteScrollContent from './infinite-scroll-content.vue';
+module.exports = {
+  InfiniteScroll,
+  InfiniteScrollContent,
+};
+
