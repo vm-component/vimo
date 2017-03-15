e371d4b5a9a96610c1aff58aeadfe695d3f4e62b
向松涛 <280304286@163.com>
2017-03-15 09:57:36 +0800

    refresher组件demo调整

--- src/views/refresher.vue
+++ src/views/refresher.vue
@@ -1,77 +1,66 @@
 <template>
-   <Page>
-     <Header>
-       <Navbar>
-         <Title>Refresher</Title>
-       </Navbar>
-     </Header>
-     <Content>
+  <Page>
+    <Header>
+      <Navbar>
+        <Title>Refresher</Title>
+      </Navbar>
+    </Header>
+    <Content>
 
-       <Refresher slot="refresher" @onRefresh="doRefresh($event)" @onPull="doPulling($event)">
-         <RefresherContent
-           pullingText="下拉刷新..."
-           refreshingText="正在刷新...">
-         </RefresherContent>
-       </Refresher>
+      <Refresher slot="refresher" @onRefresh="doRefresh($event)" @onPull="doPulling($event)">
+        <RefresherContent
+          pullingText="下拉刷新..."
+          refreshingText="正在刷新...">
+        </RefresherContent>
+      </Refresher>
 
-       <List>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-         <Item>1</Item>
-       </List>
-     </Content>
-   </Page>
+      <List>
+        <Item v-for="i in list">{{i}}</Item>
+      </List>
+    </Content>
+  </Page>
 </template>
 <style scoped lang="scss">
- .main{
+  .main {
 
- }
+  }
 </style>
 <script type="text/ecmascript-6">
-    export default{
-        name:'name',
-        data(){
-            return{}
-        },
-        props:{},
-        watch:{},
-        computed:{},
-        methods:{
-          doRefresh($event){
-            console.debug('doRefresh $event')
-
-            setTimeout(()=>{
-              $event.complete()
-            },111000)
-
-          },
-          doPulling($event){
-
+  export default{
+    name: 'name',
+    data(){
+      return {
+        i: 0,
+        list: [],
+      }
+    },
+    props: {},
+    watch: {},
+    computed: {},
+    methods: {
+      doRefresh($event){
+        console.debug('doRefresh $event')
+        let _start = this.i;
+        setTimeout(() => {
+          for (; (10 + _start) > this.i; this.i++) {
+            this.list.unshift(`item - ${this.i}`)
           }
+          // 当前异步完成
+          $event.complete();
+          console.debug('onInfinite-complete')
+        }, 500)
 
-        },
-        created:function(){},
-        mounted:function(){},
-        activated:function(){},
-        components:{}
-    }
+      },
+      doPulling($event){
+      }
+    },
+    created(){
+      for (; 15 > this.i; this.i++) {
+        this.list.unshift(`item - ${this.i}`)
+      }
+    },
+    mounted: function () {},
+    activated: function () {},
+    components: {}
+  }
 </script>
