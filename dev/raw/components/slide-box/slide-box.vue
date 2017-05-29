fe8f75c7572cffcfa8bc46d166dfb819103e6774
向松涛 <280304286@163.com>
2017-05-29 18:19:37 +0800

    slide-box组件更新

--- dev/raw/components/slide-box/slide-box.vue
+++ dev/raw/components/slide-box/slide-box.vue
@@ -1,23 +1,36 @@
 <template>
-    <div class="vm-slideBox">
+    <div class="vm-slideBox" :state="state">
         <div class="vm-slideBox-placeholder">{{placeholder}}</div>
 
         <!--绿色背景-->
-        <div class="vm-slideBox-track">
-            <div class="track-green">
-
+        <div class="vm-slideBox-track" :style="{'width':left+'px'}">
+            <div class="track-green" v-if="boxRect" :style="{'width':boxRect.width+'px'}">
+                <span v-if="state === 'inactive' || state === 'sliding' ||state === 'cancelling'"> {{placeholder}}</span>
+                <span v-if="state === 'checking'">{{checkingText}}</span>
+                <span v-if="state === 'completing'">{{completeText}}</span>
+                <span v-if="state === 'failing'">{{failText}}</span>
             </div>
         </div>
 
         <!--按钮-->
-        <div class="vm-slideBox-btn"
+        <div class="vm-slideBox-btn" :style="{'transform':'translateX('+left+'px)'}"
+             ref="slideBoxBtn"
              @touchstart="onPointerStartHandler($event)"
+             @touchmove="onPointerMoveHandler($event)"
              @touchend="onPointerEndHandler($event)"
-             @touchmove="onPointerMoveHandler($event)"></div>
+        >
+            <i class="icon-arrow-right"
+               v-if="state === 'inactive' || state === 'sliding' || state === 'cancelling'"></i>
+            <i class="icon-ok" v-if="state === 'completing'"></i>
+            <i class="icon-error" v-if="state === 'failing'"></i>
+            <i class="icon-refresh" v-if="state === 'checking'"></i>
 
+        </div>
     </div>
 </template>
 <style lang="scss">
+    $completeColor: #78c430;
+    $failColor: #ef4644;
     .vm-slideBox {
         position: relative;
         height: 52px;
@@ -45,7 +58,6 @@
                 }
             }
         }
-
         .vm-slideBox-track {
             position: absolute;
             left: 0;
@@ -58,7 +70,7 @@
                 height: 52px;
                 text-align: center;
                 font-size: 16px;
-                background-color: #78c430;
+                background-color: $completeColor;
                 color: #fff;
                 width: 100%;
             }
@@ -70,52 +82,212 @@
             width: 52px;
             height: 52px;
             cursor: pointer;
-
+            opacity: 1;
             display: flex;
             justify-content: center;
             align-items: center;
-            background: #fff url(./images/arrow.png) no-repeat center center/20px 20px;
+            background: #fff;
+            .icon-arrow-right {
+                width: 20px;
+                height: 20px;
+                background: #fff url(./images/arrow.png) no-repeat center center/20px 20px;
+            }
+            .icon-ok {
+                width: 20px;
+                height: 20px;
+                background: #fff url(./images/ok.png) no-repeat center center/20px 20px;
+            }
+            .icon-error {
+                width: 20px;
+                height: 20px;
+                background: #fff url(./images/error.png) no-repeat center center/20px 20px;
+            }
+            .icon-refresh {
+                width: 20px;
+                height: 20px;
+                background: #fff url(./images/refresh.png) no-repeat center center/20px 20px;
+                animation: revolve 2s linear infinite;
+            }
+            @keyframes revolve {
+                from {
+                    -webkit-transform: rotate(0deg);
+                    transform: rotate(0deg);
+                }
+
+                to {
+                    -webkit-transform: rotate(360deg);
+                    transform: rotate(360deg);
+                }
+            }
+
         }
+    }
+
+    .vm-slideBox[state="failing"] {
+        .vm-slideBox-track {
+            .track-green {
+                background-color: $failColor;
+            }
+        }
+    }
+
+    .vm-slideBox[state="cancelling"] {
+
+        &:after {
+            content: '';
+            position: absolute;
+            left: 0;
+            top: 0;
+            height: 100%;
+            width: 100%;
+            z-index: 1;
+            pointer-events: none;
+        }
+
+        .vm-slideBox-track {
+            transition: width ease 500ms;
+            will-change: width;
+        }
+        .vm-slideBox-btn {
+            transition: transform ease 500ms;
+            will-change: transform;
+        }
+
     }
 </style>
 <script type="text/javascript">
   /**
    *
+   *
+   * 组件的状态, 同一时间只能保持一个
+   *
+   * - inactive     // 初始状态
+   * - sliding      // 滑动状态
+   * - checking     // 正在验证
+   * - cancelling   // 释放状态
+   * - completing   // 验证通过状态
+   * - failing      // 验证失败状态
+   *
+   *
    * */
-  import { pointerCoord } from '../../util/util'
+  import { pointerCoord, clamp, transitionEnd } from '../../util/util'
+  const STATE_INACTIVE = 'inactive'         // 初始状态
+  const STATE_SLIDING = 'sliding'           // 滑动状态
+  const STATE_CHECKING = 'checking'         // 正在验证
+  const STATE_CANCELLING = 'cancelling'     // 释放(取消)状态
+  const STATE_COMPLETING = 'completing'     // 验证通过状态
+  const STATE_FAILING = 'failing'           // 验证失败状态
   export default{
     name: 'name',
-    data(){
+    data () {
       return {
-        rect: null
+        min: 0,             // 可移动的最小距离
+        max: 0,             // 可移动的最大距离
+
+        left: null,         // 向右移动数值
+
+        isReleasing: false,    // 当前输入btn释放状态
+
+        state: STATE_INACTIVE, // 初始状态
+
+        boxRect: null,      // 外容器的尺寸数据
+        btnRect: null       // 初始化时的btn的尺寸
       }
     },
     props: {
       placeholder: {
         type: String,
         default: '向右滑动验证'
+        // 请按住滑块,拖动到最右边
+      },
+      checkingText: {
+        type: String,
+        default: '正在验证'
+      },
+      completeText: {
+        type: String,
+        default: '验证通过'
+      },
+      failText: {
+        type: String,
+        default: '验证失败'
       }
     },
     watch: {},
-    computed: {},
+    computed: {
+      slideBoxBtnElement () {
+        return this.$refs.slideBoxBtn
+      }
+    },
     methods: {
+      // ------ public ------
+
+      /**
+       * 验证取消, 滑动到底部
+       * */
+      cancel () {
+        this.state = STATE_CANCELLING
+        this.left = 0
+        transitionEnd(this.slideBoxBtnElement, () => {
+          this.$nextTick(() => {
+            this.state = STATE_INACTIVE
+          })
+        })
+      },
+
+      /**
+       * 进入验证状态
+       * */
+      checking () {
+        this.state = STATE_CHECKING
+
+      },
+
+      /**
+       * 验证完成
+       * */
+      complete () {
+        this.state = STATE_COMPLETING
+      },
+
+      /**
+       * 验证失败, 等待2s后重置
+       * */
+      fail () {
+        this.state = STATE_FAILING
+      },
+
+      // ------ private------
       onPointerStartHandler ($event) {
+        if (this.state !== STATE_INACTIVE) return
+        $event.preventDefault()
+        this.state = STATE_SLIDING
         let coord = pointerCoord($event)
-        this.pointerStart = coord.x
+        this.pointerStart = coord.x - this.slideBoxBtnElement.getBoundingClientRect().left
       },
       onPointerMoveHandler ($event) {
-
+        if (this.state !== STATE_SLIDING) return
+        $event.preventDefault()
+        let coord = pointerCoord($event)
+        let left = coord.x - this.boxRect.left - this.pointerStart
+        this.left = clamp(this.min, left, this.max)
       },
-      onPointerEndHandler ($event) {
-
+      onPointerEndHandler () {
+        if (this.state !== STATE_SLIDING) return
+        if (this.left === this.max) {
+          this.$emit('onSlideEnd', this)
+        } else {
+          this.cancel()
+        }
       }
     },
     created () {},
     mounted () {
-      this.rect = this.$el.getBoundingClientRect()
-
+      this.boxRect = this.$el.getBoundingClientRect()
+      this.btnRect = this.slideBoxBtnElement.getBoundingClientRect()
+      this.max = this.boxRect.width - this.btnRect.width
     },
-    activated(){},
+    activated () {},
     components: {}
   }
 </script>
--- dev/src/example/slide-box.vue
+++ dev/src/example/slide-box.vue
@@ -2,15 +2,35 @@
     <Page>
         <Header>
             <Navbar>
-                <Title>Title</Title>
+                <Title>SlideBox</Title>
             </Navbar>
         </Header>
         <Content padding class="outer-content">
             <h5>简述</h5>
             <p>这是一个仿照淘宝注册的一个验证组件, 向右滑动到底部意味着用户确认协议可以继续向下进行.</p>
             <h5>示例</h5>
-            <SlideBox>Content</SlideBox>
+            <SlideBox @onSlideEnd="onSlideEndHandler"></SlideBox>
             <p>继续操作即视为同意 <a href="#">淘宝服务协议</a>, <a href="#">法律声明及隐私权政策</a>和 <a href="#">支付宝协议</a>, 系统将同步为您创建支付宝账户</p>
+
+            <h5>其他示例</h5>
+            <p>组件一共有以下几种状态, 且状态只能维持其一, 且 <strong>checking/cancelling/completing/failing</strong> 状态的切换由业务控制. </p>
+            <ul>
+                <li>inactive(初始状态)</li>
+                <li>sliding(滑动状态)</li>
+                <li>checking(正在验证)</li>
+                <li>cancelling(释放状态)</li>
+                <li>completing(验证通过状态)</li>
+                <li>failing(验证失败状态)</li>
+            </ul>
+
+            <p>向右滑动进入验证状态, 1s后重置</p>
+            <SlideBox @onSlideEnd="onSlideEndHandler"></SlideBox>
+
+            <p>向右滑动等待1s后验证成功</p>
+            <SlideBox @onSlideEnd="onSlideEndHandler1"></SlideBox>
+
+            <p>向右滑动等待1s后验证失败, 之后返回</p>
+            <SlideBox @onSlideEnd="onSlideEndHandler2"></SlideBox>
         </Content>
     </Page>
 </template>
@@ -27,7 +47,32 @@
     props: {},
     watch: {},
     computed: {},
-    methods: {},
+    methods: {
+      // 向右滑动进入验证状态, 4s后重置
+      onSlideEndHandler (ins) {
+        ins.checking()
+        setTimeout(() => {
+          ins.cancel()
+        }, 1000)
+      },
+      // 向右滑动等待1s后验证成功
+      onSlideEndHandler1 (ins) {
+        ins.checking()
+        setTimeout(() => {
+          ins.complete()
+        }, 1000)
+      },// 向右滑动等待1s后验证失败
+      onSlideEndHandler2 (ins) {
+        ins.checking()
+        setTimeout(() => {
+          ins.fail()
+          setTimeout(() => {
+            ins.cancel()
+          }, 1000)
+        }, 1000)
+      }
+
+    },
     created () {},
     mounted () {},
     activated () {},
