341369bdcc19c424488ed4afdbfc8b130d20929d
向松涛 <280304286@163.com>
2017-05-29 21:16:24 +0800

    slide-box组件更新, 优化动画

--- dev/raw/components/slide-box/slide-box.vue
+++ dev/raw/components/slide-box/slide-box.vue
@@ -3,39 +3,35 @@
         <div class="vm-slideBox-placeholder">{{placeholder}}</div>
 
         <!--绿色背景-->
-        <div class="vm-slideBox-track" :style="{'width':left+'px'}">
-            <div class="track-green" v-if="boxRect" :style="{'width':boxRect.width+'px'}">
-                <span v-if="state === 'inactive' || state === 'sliding' ||state === 'cancelling'"> {{placeholder}}</span>
+        <div class="vm-slideBox-track" ref="slideBoxTrack" :style="{'transform':'translateX(-'+translateX+'px)'}">
+            <div class="track-background">
                 <span v-if="state === 'checking'">{{checkingText}}</span>
                 <span v-if="state === 'completing'">{{completeText}}</span>
                 <span v-if="state === 'failing'">{{failText}}</span>
             </div>
-        </div>
-
-        <!--按钮-->
-        <div class="vm-slideBox-btn" :style="{'transform':'translateX('+left+'px)'}"
-             ref="slideBoxBtn"
-             @touchstart="onPointerStartHandler($event)"
-             @touchmove="onPointerMoveHandler($event)"
-             @touchend="onPointerEndHandler($event)"
-        >
-            <i class="icon-arrow-right"
-               v-if="state === 'inactive' || state === 'sliding' || state === 'cancelling'"></i>
-            <i class="icon-ok" v-if="state === 'completing'"></i>
-            <i class="icon-error" v-if="state === 'failing'"></i>
-            <i class="icon-refresh" v-if="state === 'checking'"></i>
-
+            <!--按钮-->
+            <div class="track-btn" ref="slideBoxBtn"
+                 @touchstart="onPointerStartHandler($event)"
+                 @touchmove="onPointerMoveHandler($event)"
+                 @touchend="onPointerEndHandler($event)">
+                <i class="icon-arrow-right"
+                   v-if="state === 'inactive' || state === 'sliding' || state === 'cancelling'"></i>
+                <i class="icon-ok" v-if="state === 'completing'"></i>
+                <i class="icon-error" v-if="state === 'failing'"></i>
+                <i class="icon-refresh" v-if="state === 'checking'"></i>
+            </div>
         </div>
     </div>
 </template>
 <style lang="scss">
-    $completeColor: #78c430;
-    $failColor: #ef4644;
+    $green: #78c430;
+    $red: #ef4644;
     .vm-slideBox {
         position: relative;
         height: 52px;
         box-shadow: 0 0 3px #999;
         background-color: #ddd;
+        overflow: hidden;
         .vm-slideBox-placeholder {
             background: -webkit-gradient(linear, left top, right top, color-stop(0, #4d4d4d), color-stop(.4, #4d4d4d), color-stop(.5, white), color-stop(.6, #4d4d4d), color-stop(1, #4d4d4d));
             -webkit-background-clip: text;
@@ -63,70 +59,81 @@
             left: 0;
             top: 0;
             height: 100%;
-            width: 0;
+            width: 100%;
             overflow: hidden;
-            .track-green {
+
+            .track-background {
                 line-height: 52px;
                 height: 52px;
                 text-align: center;
                 font-size: 16px;
-                background-color: $completeColor;
                 color: #fff;
                 width: 100%;
             }
+            .track-btn {
+                position: absolute;
+                right: 0;
+                top: 0;
+                width: 52px;
+                height: 52px;
+                cursor: pointer;
+                opacity: 1;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                background: #fff;
+                .icon-arrow-right {
+                    width: 20px;
+                    height: 20px;
+                    background: #fff url(./images/arrow.png) no-repeat center center/20px 20px;
+                }
+                .icon-ok {
+                    width: 20px;
+                    height: 20px;
+                    background: #fff url(./images/ok.png) no-repeat center center/20px 20px;
+                }
+                .icon-error {
+                    width: 20px;
+                    height: 20px;
+                    background: #fff url(./images/error.png) no-repeat center center/20px 20px;
+                }
+                .icon-refresh {
+                    width: 20px;
+                    height: 20px;
+                    background: #fff url(./images/refresh.png) no-repeat center center/20px 20px;
+                    animation: revolve 2s linear infinite;
+                }
+                @keyframes revolve {
+                    from {
+                        -webkit-transform: rotate(0deg);
+                        transform: rotate(0deg);
+                    }
+
+                    to {
+                        -webkit-transform: rotate(360deg);
+                        transform: rotate(360deg);
+                    }
+                }
+            }
         }
-        .vm-slideBox-btn {
-            position: absolute;
-            left: 0;
-            top: 0;
-            width: 52px;
-            height: 52px;
-            cursor: pointer;
-            opacity: 1;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background: #fff;
-            .icon-arrow-right {
-                width: 20px;
-                height: 20px;
-                background: #fff url(./images/arrow.png) no-repeat center center/20px 20px;
-            }
-            .icon-ok {
-                width: 20px;
-                height: 20px;
-                background: #fff url(./images/ok.png) no-repeat center center/20px 20px;
-            }
-            .icon-error {
-                width: 20px;
-                height: 20px;
-                background: #fff url(./images/error.png) no-repeat center center/20px 20px;
-            }
-            .icon-refresh {
-                width: 20px;
-                height: 20px;
-                background: #fff url(./images/refresh.png) no-repeat center center/20px 20px;
-                animation: revolve 2s linear infinite;
-            }
-            @keyframes revolve {
-                from {
-                    -webkit-transform: rotate(0deg);
-                    transform: rotate(0deg);
-                }
+    }
 
-                to {
-                    -webkit-transform: rotate(360deg);
-                    transform: rotate(360deg);
-                }
+    .vm-slideBox[state="inactive"],
+    .vm-slideBox[state="sliding"],
+    .vm-slideBox[state="checking"],
+    .vm-slideBox[state="cancelling"],
+    .vm-slideBox[state="completing"] {
+        .vm-slideBox-track {
+            .track-background {
+                background-color: $green;
             }
-
         }
     }
 
     .vm-slideBox[state="failing"] {
         .vm-slideBox-track {
-            .track-green {
-                background-color: $failColor;
+            .track-background {
+                background-color: $red;
             }
         }
     }
@@ -145,15 +152,11 @@
         }
 
         .vm-slideBox-track {
-            transition: width ease 500ms;
-            will-change: width;
-        }
-        .vm-slideBox-btn {
             transition: transform ease 500ms;
             will-change: transform;
         }
-
     }
+
 </style>
 <script type="text/javascript">
   /**
@@ -181,10 +184,13 @@
     name: 'name',
     data () {
       return {
+        timer: null,
+        unReg: null,
+        start: 0,
         min: 0,             // 可移动的最小距离
         max: 0,             // 可移动的最大距离
 
-        left: null,         // 向右移动数值
+        translateX: null,         // 向右移动数值
 
         isReleasing: false,    // 当前输入btn释放状态
 
@@ -198,7 +204,6 @@
       placeholder: {
         type: String,
         default: '向右滑动验证'
-        // 请按住滑块,拖动到最右边
       },
       checkingText: {
         type: String,
@@ -215,6 +220,9 @@
     },
     watch: {},
     computed: {
+      slideBoxTrackElement () {
+        return this.$refs.slideBoxTrack
+      },
       slideBoxBtnElement () {
         return this.$refs.slideBoxBtn
       }
@@ -227,8 +235,18 @@
        * */
       cancel () {
         this.state = STATE_CANCELLING
-        this.left = 0
-        transitionEnd(this.slideBoxBtnElement, () => {
+        this.translateX = this.max
+
+        // 从两个维度判断, 状态必须变更
+        this.timer = window.setTimeout(() => {
+          this.unReg && this.unReg()
+          this.$nextTick(() => {
+            this.state = STATE_INACTIVE
+          })
+        }, 500)
+
+        this.unReg = transitionEnd(this.slideBoxTrackElement, () => {
+          this.timer && window.clearTimeout(this.timer)
           this.$nextTick(() => {
             this.state = STATE_INACTIVE
           })
@@ -268,25 +286,23 @@
         if (this.state !== STATE_SLIDING) return
         $event.preventDefault()
         let coord = pointerCoord($event)
-        let left = coord.x - this.boxRect.left - this.pointerStart
-        this.left = clamp(this.min, left, this.max)
+        let left = coord.x - this.boxRect.left - this.pointerStart >> 0
+        this.translateX = this.max - clamp(this.min, left, this.max)
       },
       onPointerEndHandler () {
         if (this.state !== STATE_SLIDING) return
-        if (this.left === this.max) {
+        if (this.translateX === this.min) {
           this.$emit('onSlideEnd', this)
         } else {
           this.cancel()
         }
       }
     },
-    created () {},
     mounted () {
       this.boxRect = this.$el.getBoundingClientRect()
       this.btnRect = this.slideBoxBtnElement.getBoundingClientRect()
-      this.max = this.boxRect.width - this.btnRect.width
-    },
-    activated () {},
-    components: {}
+      this.translateX = this.max = this.boxRect.width - this.btnRect.width >> 0
+    }
   }
+
 </script>
