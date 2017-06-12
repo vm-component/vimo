d9536d00f9591d529aa3d18143fdce49964e12a1
向松涛 <280304286@163.com>
2017-06-12 15:04:24 +0800

    textarea增加高度自适应

--- dev/raw/components/input/mixin.vue
+++ dev/raw/components/input/mixin.vue
@@ -25,6 +25,9 @@
                     :placeholder="placeholder"
                     :disabled="disabled"
                     :readonly="readonly"
+                    :autofocus="autofocus"
+                    :maxlength="maxlength"
+                    :rows="rows"
                     ref="textarea"
                     @blur="inputBlurred($event)"
                     @focus="inputFocused($event)"
@@ -104,23 +107,22 @@
    * 网址URL | url             | 网址URL, 必须以(https,http,ftp,rtsp,mms)开头
    *
    *
-   *
-   *
-   *
-   * @props {Boolean} [clearInput] - 如果为true, 当输入值的时候一个清除按钮会在input右边出现, 点击按钮则清除输入. textarea没有这个特性
-   * @props {Boolean} [clearOnEdit] -  如果为true, 当再次输入的时候会清空上次的输入, 如果type为password时默认为true, 其余情况默认为false, 默认值的变更, 需要js控制
-   * @props {Boolean} [disabled] -  如果为true, 用户无法输入
-   * @props {Number} [max] -  设置最大值, 只对type=number有效
-   * @props {Number} [min] -  设置最小值, 只对type=number有效
-   * @props {Number} [step] - 设置数字变化的阶梯值, 只对type=number有效
-   * @props {String} [mode='ios'] - 当前平台
-   * @props {String} [placeholder] - 占位文字
-   * @props {Boolean} [readonly] - 只读模式, 不能修改
-   * @props {String} [type='text'] - 输入的类型: "text", "password", "email", "number", "search", "tel", or "url"
-   * @props {*} [value] - 内容输入值
-   * @props {Number} [debounce=0] - 触发间隔
-   * @props {RegExp} [regex] - 自定义正则
-   * @props {Boolean} [check] -  是否check输入结果, 如果regex有值, 则开启, 否自关闭. 如果check开启, 但是regex无值, 则使用内置判断. 默认关闭check, check只是作为内部正误标示, 对外提交不起作用, 如果点击能知道各个input的状态, 需要在dom中search'ng-invalid'类名, 这样的话, 验证位置就会统一.
+   * @props {Boolean} [clearInput]      - 如果为true, 当输入值的时候一个清除按钮会在input右边出现, 点击按钮则清除输入. textarea没有这个特性
+   * @props {Boolean} [clearOnEdit]     - 如果为true, 当再次输入的时候会清空上次的输入, 如果type为password时默认为true, 其余情况默认为false, 默认值的变更, 需要js控制
+   * @props {Boolean} [disabled]        - 如果为true, 用户无法输入
+   * @props {Number} [max]              - 设置最大值, 只对type=number有效
+   * @props {Number} [maxlength]        - 设置最大值, 只对textarea有效
+   * @props {Number} [rows=3]             - 设置行数, 只对textarea有效
+   * @props {Number} [min]              - 设置最小值, 只对type=number有效
+   * @props {Number} [step]             - 设置数字变化的阶梯值, 只对type=number有效
+   * @props {String} [mode='ios']       - 当前平台
+   * @props {String} [placeholder]      - 占位文字
+   * @props {Boolean} [readonly]        - 只读模式, 不能修改
+   * @props {String} [type='text']      - 输入的类型: "text", "password", "email", "number", "search", "tel", or "url"
+   * @props {*} [value]                 - 内容输入值
+   * @props {Number} [debounce=0]       - 触发间隔
+   * @props {RegExp} [regex]            - 自定义正则
+   * @props {Boolean} [check]           - 是否check输入结果, 如果regex有值, 则开启, 否自关闭. 如果check开启, 但是regex无值, 则使用内置判断. 默认关闭check, check只是作为内部正误标示, 对外提交不起作用, 如果点击能知道各个input的状态, 需要在dom中search'ng-invalid'类名, 这样的话, 验证位置就会统一.
    *
    *
    *
@@ -178,9 +180,27 @@
       max: [Number],
 
       /**
+       * 设置最大值, 只对textarea有效
+       * */
+      maxlength: [Number],
+
+      /**
+       * 设置行数, 只对textarea有效
+       * */
+      rows: {
+        type: Number,
+        default: 3
+      },
+
+      /**
        * 设置最小值, 只对type=number有效
        * */
       min: [Number],
+
+      /**
+       * 自动focus
+       * */
+      autofocus: Boolean,
 
       /**
        * 设置数字变化的阶梯值, 只对type=number有效
@@ -242,8 +262,29 @@
 
     },
     methods: {
+
+      // -------- public --------
+      /**
+       * @function update
+       * @description
+       * 更新textarea组件
+       * */
+      update () {
+        this.typeValue === 'textarea' && Autosize.update(this.textareaElement)
+      },
+      /**
+       * @function destroy
+       * @description
+       * 销毁textarea组件
+       * */
+      destroy () {
+        this.typeValue === 'textarea' && Autosize.destroy(this.textareaElement)
+      },
+
+      // -------- private --------
       /**
        * 执行验证, 如果错误则设置ng-invalid, 正确则设置ng-valid
+       * @private
        * */
       verification () {
         if (!this.checkValue) return
@@ -386,6 +427,7 @@
 
       /**
        * 键盘按下事件
+       * @private
        * */
       onKeydown () {
         if (this.clearOnEditValue) {
@@ -393,6 +435,9 @@
         }
       },
 
+      /**
+       * @private
+       * */
       checkClearOnEdit () {
         if (!this.clearOnEditValue) {
           return
@@ -410,6 +455,7 @@
 
       /**
        * 点击清除输入项
+       * @private
        * */
       clearTextInput () {
         this.inputValue = ''
@@ -422,6 +468,7 @@
 
       /**
        *  设置父组件Item被点中时的class
+       *  @private
        */
       setItemHasFocusClass (isFocus) {
         if (this.itemComponent) {
@@ -431,6 +478,7 @@
 
       /**
        *  设置父组件Item有值时的class
+       *  @private
        */
       setItemHasValueClass () {
         if (this.itemComponent) {
@@ -440,6 +488,7 @@
 
       /**
        * 判断input是否有value
+       * @private
        * */
       hasValue () {
         const inputValue = this.inputValue
