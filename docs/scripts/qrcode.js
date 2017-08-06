c5747b523b5cf7c9579e4c172b9e5e3ee15365e3
Hisang <280304286@163.com>
2017-08-06 12:41:14 +0800

    refresh docs

--- docs/scripts/qrcode.js
+++ docs/scripts/qrcode.js
@@ -632,15 +632,14 @@ function QRBitBuffer() {
                 el.onerror = fOnError;
                 el.onload = fOnSuccess;
                 el.src = "data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="; // the Image contains 1px data.
-                return;
+
             } else if (self._bSupportDataURI === true && self._fSuccess) {
                 self._fSuccess.call(self);
             } else if (self._bSupportDataURI === false && self._fFail) {
                 self._fFail.call(self);
             }
-        };
-        
-        /**
+        }
+      /**
          * Drawing QRCode by using canvas
          * 
          * @constructor
