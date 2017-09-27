8b683e3a1750563844424b76eee502b8bd600e2f
xiangsongtao <280304286@163.com>
2017-09-27 16:14:19 +0800

    docs: Badge组件文档整理, 增加demo链接

--- components/badge/badge.vue
+++ components/badge/badge.vue
@@ -16,13 +16,14 @@
    *
    * ## 其他 / Badge徽章组件
    *
-   * 就是在项目右上角指示数字的标志
+   * 徽章组件, 可在Item中组合使用.
    *
-   * @props {String} color - 颜色
+   * @props {String} [color] - 颜色
    * @props {String} [mode='ios'] - 模式
    *
-   * $slot 空 - 默认嵌入全部
+   * @slot 空 - 默认嵌入全部
    *
+   * @demo #/badge
    * */
   export default {
     name: 'Badge',
