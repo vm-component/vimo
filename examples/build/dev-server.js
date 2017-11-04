8746ab67a79690a9a6195d0df5c2ba3987d20530
Eric <nostaff@sina.com>
2017-11-04 16:37:38 +0800

    refactor: build-server

--- examples/build/dev-server.js
+++ examples/build/dev-server.js
@@ -64,8 +64,7 @@ app.use(hotMiddleware)
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
-var ip = getIPAdress()
-var uri = 'http://' + ip + ':' + port
+var uri = 'http://localhost:' + port
 
 devMiddleware.waitUntilValid(function () {
   console.log('> Listening at ' + uri + '\n')
@@ -82,16 +81,3 @@ module.exports = app.listen(port, function (err) {
     opn(uri)
   }
 })
-
-function getIPAdress () {
-  var interfaces = require('os').networkInterfaces()
-  for (var devName in interfaces) {
-    var iface = interfaces[devName]
-    for (var i = 0; i < iface.length; i++) {
-      var alias = iface[i]
-      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
-        return alias.address
-      }
-    }
-  }
-}
