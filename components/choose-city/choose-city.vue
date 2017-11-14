1b625d96619f5baf15fd1ac286f9953df05b7eb1
xiangsongtao <280304286@163.com>
2017-11-14 15:46:15 +0800

    chore: choose-city组件整理

---
+++ components/choose-city/choose-city.vue
@@ -0,0 +1,300 @@
+<template>
+    <transition
+            name="fade-right"
+            @before-enter="beforeEnter"
+            @after-enter="afterEnter"
+            @before-leave="beforeLeave"
+            @after-leave="afterLeave">
+        <article class="vm-city-selector" v-show="isActive">
+            <div class="city-shortcut" ref="cityShortcut"
+                 @touchstart="onTouchShortcut"
+                 @touchmove="onTouchShortcut">
+                <div class="shortcut__item" :data-id="item.name" v-for="item in shortcutList">{{item.name}}</div>
+            </div>
+            <div class="city-selector-container" ref="content">
+                <div class="city-selector">
+                    <!--定位服务-->
+                    <article class="city-selector__group" v-if="showLocatedCity">
+                        <section sticky color="light" class="city-selector__group--header">
+                            <span>你所在的地区</span>
+                        </section>
+                        <section class="city-selector__group--hotBox">
+                            <div class="hotBox__wrap" @click="selectCity(currentCity)">
+                                <div class="hotBox__wrap--city">{{currentCity.city}}</div>
+                            </div>
+                        </section>
+                    </article>
+                    <!--热门城市-->
+                    <article class="city-selector__group" v-if="showHotCities && hotCities.length>0">
+                        <section sticky color="light" class="city-selector__group--header" :id="'★' | getClassifyId">
+                            <span>热门城市</span>
+                        </section>
+                        <section class="city-selector__group--hotBox">
+                            <div class="hotBox__wrap" @click="selectCity(item)" v-for="item in hotCities"
+                                 :key="item.adCode">
+                                <div class="hotBox__wrap--city">{{item.city}}</div>
+                            </div>
+                        </section>
+                    </article>
+                    <!--城市列表-->
+                    <article class="city-selector__group" v-for="(classify,index) in cityList" :key="index">
+                        <!--sticky-->
+                        <section class="city-selector__group--header" :id="classify.letter | getClassifyId">{{classify.letter}}
+                        </section>
+                        <section class="city-selector__group--item" v-for="item in classify.cities"
+                                 :key="item.adCode"
+                                 @click="selectCity(item)">
+                            <div>{{item.city}}</div>
+                        </section>
+                    </article>
+                </div>
+            </div>
+        </article>
+    </transition>
+</template>
+<style lang="less">
+    @import "./choose-city.less";
+    @import "../themes/transition/fade-right.less";
+</style>
+<script type="text/javascript">
+  import { pointerCoord, clamp } from '../util/util'
+  import popupExtend from '../util/popup-extend'
+
+  export default {
+    name: 'ChooseCity',
+    extends: popupExtend,
+    props: {
+      showLocatedCity: Boolean,
+      showHotCities: {
+        type: Boolean,
+        default: true
+      },
+      hotCities: {
+        type: Array,
+        default () {
+          return require('./hot-cities.json')
+        }
+      },
+      cities: {
+        type: Array,
+        default () {
+          return require('./cities.json')
+        }
+      },
+      ak: {
+        type: String,
+        default: '8d1ba642a3a3046d1ee087e0f8b490a2'
+      },
+      onDismiss: {
+        type: Function,
+        default: function () {}
+      }
+    },
+    data () {
+      return {
+        currentCity: {city: '正在定位', adcode: null},         // 当前城市，如果开启开启了定位的话
+
+        shortcutTop: 0,         // shortcutElement距离页面顶部的距离
+        shortcutList: []        // cityShortcut 的数组 A->Z
+      }
+    },
+    filters: {
+      getClassifyId (name) {
+        return 'city-' + name.substr(0, 1)
+      }
+    },
+    computed: {
+      shortcutElement () {
+        return this.$refs.cityShortcut
+      },
+      contentComponent () {
+        return this.$refs.content
+      },
+      cityList () {
+        return this.sortCityList(this.cities)
+      }
+    },
+    methods: {
+      /**
+       * 触摸开始和移动
+       * @private
+       * */
+      onTouchShortcut (ev) {
+        ev.preventDefault()
+        ev.stopPropagation()
+        let index = this.getSelectedIndex(ev)
+        this.contentComponent.scrollTo(0, this.shortcutList[index].top, 0)
+      },
+
+      /**
+       * 由数据初始化 cityShortcut
+       * */
+      initShortCut () {
+        if (this.showHotCities) {
+          this.shortcutList.push({
+            name: '★',
+            top: 0
+          })
+        }
+        this.cityList.forEach((group) => {
+          var name = group.letter.substr(0, 1)
+          this.shortcutList.push({
+            name: name,
+            top: 0
+          })
+        })
+      },
+
+      /**
+       * 根据传入的ev值获取当前在Shortcut上点击的是第几个item
+       * @private
+       * */
+      getSelectedIndex (ev) {
+        let point = pointerCoord(ev)
+        let index = ((point.y - this.shortcutTop) / 16) >> 0
+        index = clamp(0, index, this.shortcutList.length - 1)
+        return index
+      },
+
+      /**
+       * 点击选择城市
+       * @private
+       * */
+      selectCity (result) {
+        if (result.city && result.adCode) {
+          this.onDismiss(JSON.parse(JSON.stringify(result)))
+          this.dismiss()
+        }
+      },
+
+      /**
+       * 获取当前位置
+       * TODO: 需要审查
+       * @private
+       * */
+      getCurrentLocation () {
+        function getScript (mapJsUrl) {
+          return new Promise((resolve, reject) => {
+            let sc = document.createElement('script')
+            sc.type = 'text/javascript'
+            sc.src = mapJsUrl
+            sc.onload = sc.onreadystatechange = function () {
+              if (!this.readyState || /^(loaded|complete)$/.test(this.readyState)) {
+                resolve()
+                sc.onload = sc.onreadystatechange = null
+              }
+            }
+            sc.onerror = function (err) {
+              reject(err)
+              sc.onerror = null
+            }
+            document.body.appendChild(sc)
+          })
+        }
+
+        return new Promise((resolve, reject) => {
+          let getCurrentPosition = () => {
+            new window.AMap.Geolocation().getCurrentPosition((status, result) => {
+              if (status === 'complete') {
+                resolve(result)
+              } else {
+                reject(status)
+              }
+            })
+          }
+
+          if (typeof window.AMap === 'undefined') {
+            getScript(`//webapi.amap.com/maps?v=1.3&key=${this.$options.$data.ak}`).then(() => {
+              window.setTimeout(function () {
+                if (!window.AMap || !window.AMap.Map) { return }
+                let map = new window.AMap.Map('')
+                map.plugin('AMap.Geolocation', () => {
+                  /**
+                   * @param {object} posOptions - 传入参数
+                   * @param {boolean} [posOptions.enableHighAccuracy=true] - 是否使用高精度定位，默认:true
+                   * @param {number} [posOptions.timeout=10000] - 超过10秒后停止定位，默认：无穷大
+                   * @param {number} 9posOptions.maximumAge=00 - 定位结果缓存0毫秒，默认：0
+                   * */
+                  getCurrentPosition()
+                })
+              }, 100)
+            }, (err) => {
+              reject(err)
+            })
+          } else {
+            getCurrentPosition()
+          }
+        })
+      },
+
+      /**
+       * 将传入的一维数据按照首字母整理，用于显示
+       * @private
+       * */
+      sortCityList (cities) {
+        // hash object for filter
+        let store = {}
+        let _cities = []
+        cities.forEach((city) => {
+          let letter = city.spell.charAt(0).toUpperCase()
+          if (store[letter]) {
+            store[letter].push(city)
+          } else {
+            store[letter] = [city]
+          }
+        })
+
+        for (let key in store) {
+          _cities.push({
+            'letter': key,
+            'cities': store[key]
+          })
+        }
+
+        // TODO 字母排序
+//        _cities.sort(function (a, b) {
+//          return a.letter > b.letter
+//        })
+
+        return _cities
+      },
+
+      /**
+       * 初始化获取当前城市
+       * @private
+       * */
+      initLocatedCity () {
+        if (this.showLocatedCity) {
+          this.getCurrentLocation().then((data) => {
+            if (data && data.addressComponent) {
+              this.currentCity = {
+                adCode: data.addressComponent.adcode,
+                city: data.addressComponent.city
+              }
+            }
+          }, () => {
+            this.currentCity = {
+              adCode: null,
+              city: '定位失败'
+            }
+          })
+        }
+      }
+    },
+    created () {
+      this.initLocatedCity()
+      this.initShortCut()
+    },
+    mounted () {
+      this.$on('popup:afterEnter', () => {
+        let clientHeight = this.shortcutElement.offsetHeight || 0
+        let docHieght = window.innerHeight || 0
+        this.shortcutTop = (docHieght - clientHeight) / 2
+        this.shortcutList.forEach((item) => {
+          let el = document.getElementById('city-' + item.name)
+          item.top = el.offsetTop + 1
+        })
+      })
+    }
+  }
+</script>
--- components/choose-city/index.js
+++ components/choose-city/index.js
@@ -27,69 +27,45 @@
  *    })
  *  }
  */
-import Modal from '../modal/index'
-import cityComponent from './city.vue'
+/**
+ * @function present
+ * @description
+ * 开启ChooseCity组件
+ * @param {Boolean}   options - 传入参数
+ * @param {Boolean}   [options.showLocatedCity=false]   - 是否显示当前定位城市，默认 false
+ * @param {Boolean}   [options.showHotCities=true]      - 是否显示热门城市，默认 true
+ * @param {Boolean}   [options.showBackdrop=true]       - modal 是否显示背景色
+ * @param {Boolean}   [options.enableBackdropDismiss=true]- modal 点击背景是否关闭
+ * @param {Array}     [options.cities]                  - 城市数据，默认 本地数据
+ * @param {Array}     [options.cities.city]             - 城市名称
+ * @param {Array}     [options.cities.adCode]           - 城市编码
+ * @param {Array}     [options.cities.spell]            - 城市拼音
+ * @param {Array}     [options.hotCities]               - 热门城市，默认 本地数据
+ * @param {Array}     [options.hotCities.city]          - 城市名称
+ * @param {Array}     [options.hotCities.adCode]        - 城市编码
+ * @param {Array}     [options.hotCities.spell]         - 城市拼音
+ * @param {String}    [options.mode='ios']              - 模式, 可以是如下取值: ios, md, fade, zoom, fade-right
+ * @param {Boolean}   [options.isH5=false']             - 是否强制使用H5模式，默认是自动的
+ * @param {String}    [options.ak='8d1b...90a2']- 如果是H5模式并且开启了'当前定位城市'，则使用高德地图定位，这个是AK
+ * @param {Function}  [options.onDismiss]               - 当选择点击后的回调，传入参数是当前选择的城市
+ * */
+import ChooseCityComponent from './choose-city.vue'
+import GeneratePopUpInstance from '../util/GeneratePopUpInstance.js'
 
-const ChooseCity = {
-  /**
-   * @function present
-   * @description
-   * 开启ChooseCity组件
-   * @param {Boolean}   options - 传入参数
-   * @param {Boolean}   [options.showLocatedCity=false]   - 是否显示当前定位城市，默认 false
-   * @param {Boolean}   [options.showHotCities=true]      - 是否显示热门城市，默认 true
-   * @param {Boolean}   [options.showBackdrop=true]       - modal 是否显示背景色
-   * @param {Boolean}   [options.enableBackdropDismiss=true]- modal 点击背景是否关闭
-   * @param {Array}     [options.cities]                  - 城市数据，默认 本地数据
-   * @param {Array}     [options.cities.city]             - 城市名称
-   * @param {Array}     [options.cities.adCode]           - 城市编码
-   * @param {Array}     [options.cities.spell]            - 城市拼音
-   * @param {Array}     [options.hotCities]               - 热门城市，默认 本地数据
-   * @param {Array}     [options.hotCities.city]          - 城市名称
-   * @param {Array}     [options.hotCities.adCode]        - 城市编码
-   * @param {Array}     [options.hotCities.spell]         - 城市拼音
-   * @param {String}    [options.mode='ios']              - 模式, 可以是如下取值: ios, md, fade, zoom, fade-right
-   * @param {Boolean}   [options.isH5=false']             - 是否强制使用H5模式，默认是自动的
-   * @param {String}    [options.ak='8d1b...90a2']- 如果是H5模式并且开启了'当前定位城市'，则使用高德地图定位，这个是AK
-   * @param {Function}  [options.onDismiss]               - 当选择点击后的回调，传入参数是当前选择的城市
-   * */
-  present (options) {
-    let finalOptions = {}
-    let defaultOptions = {
-      showLocatedCity: false, // 是否显示当前定位城市，默认 false
-      showHotCities: true, // 是否显示热门城市，默认 true
-      cities: require('./cities.json'), // 城市数据，默认 本地数据
-      hotCities: require('./hot-cities.json'), // 热门城市，默认 本地数据
-      mode: 'fade-right', // 模式 可以是如下取值: ios, md, fade, zoom, fade-right
-      showBackdrop: true, // modal 是否显示背景色
-      enableBackdropDismiss: true, // modal 点击背景是否关闭
-      isH5: false, // 是否强制使用H5模式
-      ak: '8d1ba642a3a3046d1ee087e0f8b490a2', // 如果是H5模式并且开启了'当前定位城市'，则使用高德地图定位，这个是AK
-      onDismiss () {}
-    }
-    Object.assign(finalOptions, defaultOptions, options)
+class ChooseCityInstance extends GeneratePopUpInstance {
+  normalizeOptions (options) {
+    // 进行历史记录
+    options.recordInHistory = true
+    return options
+  }
 
-    let isHandled =
-      !options.isH5 &&
+  isPresentHandled (options) {
+    return !options.isH5 &&
       window.VM &&
       window.VM.platform &&
       window.VM.platform.chooseCity &&
       window.VM.platform.chooseCity(options)
-    if (!isHandled) {
-      Modal.present({
-        mode: finalOptions.mode,
-        component: cityComponent,
-        data: {
-          showLocatedCity: finalOptions.showLocatedCity,
-          showHotCities: finalOptions.showHotCities,
-          hotCities: finalOptions.hotCities,
-          cities: finalOptions.cities
-        },
-        showBackdrop: finalOptions.showBackdrop,
-        enableBackdropDismiss: finalOptions.enableBackdropDismiss,
-        onDismiss: finalOptions.onDismiss
-      })
-    }
   }
 }
-export default ChooseCity
+
+export default new ChooseCityInstance(ChooseCityComponent, 'modalPortal')
