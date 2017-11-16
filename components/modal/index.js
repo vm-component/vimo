1636756d148152b4523773001e4009ca6f6f3bb0
xiangsongtao <280304286@163.com>
2017-11-16 17:36:33 +0800

    perf: Modal组件重构

--- components/modal/index.js
+++ components/modal/index.js
@@ -1,1 +1,184 @@
-export { default } from './modal'
+/**
+ * @component Modal
+ * @description
+ *
+ * ## 弹出层 / Modal弹出页组件
+ *
+ * ## 补充
+ *
+ * Model组件用于当前页面的补充, 相当于在当前页面上再来弹一个页面, 这里并不改变路由, 但是会改变H5 history的popstate, 每打开一个Modal则新增一个历史记录, 可以通过后退关闭最后一次打开的modal.
+ *
+ * ## 注意点
+ *
+ * - 在modal中如果跳转到另一页之前希望能先关闭当前modal再操作
+ * - modal不会再url中留下记录
+ * - modal只是装菜的盘子, 盘子中的菜通过`component`传入, 数据通过`data`传入,
+ *    - this.$options.$data.username 获取数据
+ *    - this.username 也能获取到数据, 注意别冲突
+ * - `onDismiss`会在modal关闭后触发.
+ * - 开启的页面就是完整的Page页面, 别无其他
+ *
+ * ### 动画
+ *
+ * mode有自己的默认动画, 但是如果想自定义动画, 可以在mode中传入你自定义的动画名称. 另外, 默认自带的动画```animateName```有:
+ *
+ * - 空: 下部向上渐变
+ * - zoom: 缩放
+ * - fade: 渐变
+ * - fade-right: 右侧向左渐变
+ *
+ * @usage
+ * // 开启
+ * openModal () {
+ *        this.$modal.present({
+ *          animateName: 'zoom',
+ *          component: modalPageComponent,
+ *          data: {hello: 'Page1Data'},
+ *          showBackdrop: true,
+ *          enableBackdropDismiss: true,
+ *          onDismiss (data) {
+ *            console.debug('得到了modal1的关闭信息')
+ *            console.debug(JSON.stringify(data))
+ *          }
+ *    })
+ * },
+ *
+ * // 关闭
+ * closeModal () {
+ *        this.$modal.dismiss({
+ *          result: 'modal 1 dismissed success!'
+ *        })
+ * }
+ * @demo #/modal
+ *
+ * */
+
+import Vue from 'vue'
+import registerListener from '../util/register-listener'
+import modalComponent from './modal.vue'
+import getInsertPosition from '../util/get-insert-position'
+
+let modalArr = []       // modal实例的堆栈
+let unRegisterUrlChange = []
+let isModalEnable = true // 当modal处于打开过程和关闭过程中时为false, 其余状态为true
+
+/**
+ * 获取实例
+ * @private
+ */
+function ModalFactory (options) {
+  const Modal = Vue.extend(modalComponent)
+  options.dismissOnPageChange = false
+  options.recordInHistory = false
+  return new Modal({
+    el: getInsertPosition('modalPortal').appendChild(
+      document.createElement('div')
+    ),
+    propsData: options
+  })
+}
+
+/**
+ * 开启Modal方法
+ * 如果不懂想下: 桌子(页面)/菜盘(modal)/菜(template)的关系, 开启后获取Modal实例, 并将template初始化后挂在到Modal上, 然后注册urlChange事件. 在之后记录开启的Modal信息,
+ * 然后执行modal实例的_present开启.
+ *
+ * @param {Object} options
+ * @param {String} [options.animateName] - modal开闭动画, 可以是: 空/zoom/fade/fade-enter
+ * @param {object} options.component - modal页面, 不支持异步
+ * @param {object} [options.data] - 传给modal的数据, 数据会写到component的data属性中, 当然, 这个是响应式的.
+ * @param {function} [options.onDismiss] - 关闭model执行的操作, data是关闭时传入的参数
+ * @param {Boolean} [options.showBackdrop=true] - 显示backdrop
+ * @param {Boolean} [options.enableBackdropDismiss=true] - 点击backdrop是否关闭
+ * @example
+ * 传入参数示例:
+ * {
+ *  component:require('*.vue'),  // modal页面
+ *  data:{...},            // 传给modal的数据
+ *  onDismiss(data){....},      // 关闭model执行的操作, data是关闭时传入的参数
+ * }
+ *
+ * 子页面通过
+ * - this.$options.$data.username 获取数据
+ * - this.username 也能获取到数据, 注意别冲突
+ *
+ * */
+function present (options = {}) {
+  isModalEnable = false
+  return new Promise((resolve) => {
+    let modalInstance = ModalFactory(options)
+    let presentPromise = modalInstance.present()
+    // 增加浏览器历史记录
+    window.history.pushState(
+      {
+        id: new Date().getTime(),
+        name: `Modal-${modalInstance._uid}`
+      },
+      '',
+      ''
+    )
+
+    // record
+    modalArr.push(modalInstance)
+
+    // 如果是第一次进入则监听url变化
+    if (unRegisterUrlChange.length === 0) {
+      registerListener(
+        window,
+        'popstate',
+        function () {
+          // 避免  window.history.back() 的影响
+          isModalEnable && dismiss(null, false)
+        },
+        {},
+        unRegisterUrlChange
+      )
+    }
+
+    presentPromise.then(() => {
+      isModalEnable = true
+      resolve()
+    })
+  })
+}
+
+/**
+ * 全局注册dismiss方法
+ * dismiss关闭最后一次打开的Modal, 并执行onDismiss函数, 就酱, 因为, modal是覆盖式的显示在页面上, 即使给定关闭的modal名字, 也无使用意义.
+ * `dataBack`数据将由外部`onDismiss`接收
+ * @param {*} [dataBack] -  modal调用dismiss传递向外的数据
+ * @param {Boolean} [changeLocalHistory=true] -  是否改变本地历史记录, 默认为true
+ * */
+function dismiss (dataBack, changeLocalHistory = true) {
+  isModalEnable = false
+
+  if (changeLocalHistory) {
+    window.history.back()
+  }
+
+  return new Promise(resolve => {
+    // 总是关闭最后一次创建的modal
+    let lastModalInstance = modalArr.pop()
+    // 如果是最后一个则解绑urlChange
+    if (modalArr.length === 0) {
+      unRegisterAllListener()
+    }
+
+    lastModalInstance.dismiss(dataBack).then(() => {
+      isModalEnable = true
+      // 执行注册的onDismiss回调
+      resolve()
+    })
+  })
+}
+
+// 基础全部监听
+function unRegisterAllListener () {
+  unRegisterUrlChange.forEach(item => item && item())
+  unRegisterUrlChange = []
+}
+
+export default {
+  present,
+  dismiss
+}
--- components/modal/modal.vue
+++ components/modal/modal.vue
@@ -6,33 +6,38 @@
                 :bdClick="bdClick"
                 :isActive="isActive"></Backdrop>
         <transition
-                :name="transitionClass"
+                :name="animateName"
                 @before-enter="beforeEnter"
                 @after-enter="afterEnter"
                 @before-leave="beforeLeave"
                 @after-leave="afterLeave">
             <div class="modal-wrapper" v-show="isActive">
                 <!--page in here-->
-                <div class="modalPageLoadPort" ref="modalViewport"></div>
+                <component :is="thisComponent" :data="data"></component>
             </div>
         </transition>
     </div>
 </template>
 <script type="text/javascript">
-  import Vue from 'vue'
   import Backdrop from '../backdrop'
-  import * as appComponentManager from '../util/appComponentManager'
-
-  const NOOP = () => {}
+  import popupExtend from '../util/popup-extend'
+  import prepareComponent from '../util/prepare-component'
 
   export default {
     name: 'Modal',
+    extends: popupExtend,
+    provide () {
+      const _this = this
+      return {
+        modalComponent: _this
+      }
+    },
     props: {
       animateName: String,
       // 放入的页面组件
       component: [Object, String, Function, Promise],
       // 传递给页面的数据
-      pageData: Object,
+      data: Object,
       onDismiss: Function,
       showBackdrop: {
         type: Boolean,
@@ -45,123 +50,33 @@
     },
     data () {
       return {
-        enabled: false,
         bdDismiss: false,
-        isActive: false,
-
-        // promise
-        presentCallback: NOOP,
-        dismissCallback: NOOP
+        thisComponent: null
       }
     },
     computed: {
-      transitionClass () {
-        return this.animateName ? `modal-${this.animateName}` : `modal`
-      },
       modalViewportElement () {
         return this.$refs.modalViewport
       }
     },
     methods: {
-      /**
-       * 点击backdrop
-       * @private
-       * */
       bdClick () {
         if (this.enabled && this.enableBackdropDismiss) {
           this.dismiss()
         }
       },
 
-      /**
-       * Animate Hooks
-       * */
-      beforeEnter () {
-        this.enabled = false // 不允许过渡中途操作
-        this.$app && this.$app.setEnabled(false, 400)
-      },
-      afterEnter () {
-        this.presentCallback()
-        this.enabled = true
-      },
-      beforeLeave () {
-        this.enabled = false
-        this.$app && this.$app.setEnabled(false, 400)
-      },
-      afterLeave () {
-        this.dismissCallback()
-        // 删除DOM
-        this.$el.remove()
-        this.enabled = true
-      },
-
-      /**
-       * 开启关闭值操作当前的组件
-       * */
-      present () {
-        this.isActive = true
-        // 在App组件中记录, 便于devtool中debug
-        // add comp in app comp
-        appComponentManager.addChild(this)
-        return new Promise((resolve) => { this.presentCallback = resolve })
-      },
-      dismiss (dataBack) {
-        if (this.isActive) {
-          this.isActive = false
-          this.onDismiss && this.onDismiss(dataBack)
-          if (!this.enabled) {
-            this.$nextTick(() => {
-              this.dismissCallback()
-              this.$el.remove()
-              this.enabled = true
-            })
-          }
-
-          // 在App组件中记录, 便于devtool中debug
-          appComponentManager.removeChild(this)
-          return new Promise((resolve) => { this.dismissCallback = resolve })
-        } else {
-          return new Promise((resolve) => { resolve() })
-        }
-      },
-      init (component) {
-        // 页面挂载
-        const Component = Vue.extend(component)
-        // TODO: 这部分和Content组件相关, Content组件需要调整
-        window.setTimeout(() => {
-          this.$nextTick(() => {
-            // eslint-disable-next-line no-new
-            var PageComponent = new Component({
-              el: this.modalViewportElement,
-              data: this.pageData,
-              $data: this.pageData // 为了兼容
-            })
-            this.$children.push(PageComponent)
-          })
-        }, 0)
+      beforeDismiss (data) {
+        this.onDismiss && this.onDismiss(data)
       }
     },
     created () {
-      let getType = (val) => Object.prototype.toString.call(val).match(/^(\[object )(\w+)\]$/i)[2].toLowerCase()
-      if (getType(this.component) === 'object') {
-        this.init(this.component)
-      } else if (getType(this.component) === 'function') {
-        this.component((component) => {
-          this.init(component)
-        })
-      } else if (getType(this.component) === 'promise') {
-        this.component.then((component) => {
-          this.init(component)
-        })
-      } else if (getType(this.component) === 'string') {
-        // 如果 this.component 是html模板string的话
-        this.htmlComponent = this.component
-      } else {
-          /* istanbul ignore if */
-        if (process.env.NODE_ENV !== 'production') {
-          console.error('props of "component" must pass in right value!')
-        }
-      }
+      prepareComponent(this.component).then((component) => {
+        this.thisComponent = component
+      }, () => {
+        // 如果不是 Component 则当做 html字符串处理
+        this.thisComponent = this.component
+      })
     },
     components: {
       'Backdrop': Backdrop
@@ -169,7 +84,10 @@
   }
 </script>
 <style lang="less">
-    @import "modal.less";
-    @import "modal.ios.less";
-    @import "modal.md.less";
+    @import "./modal.less";
+    @import "./modal.ios.less";
+    @import "./modal.md.less";
+    @import "../themes/transition/fade-bottom.less";
+    @import "../themes/transition/fade-right.less";
+    @import "../themes/transition/zoom.less";
 </style>
