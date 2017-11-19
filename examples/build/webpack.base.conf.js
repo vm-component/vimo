b5ac62c1f1a34cbb5a5156677ae807579cd4b1c0
Eric <nostaff@sina.com>
2017-11-19 13:47:29 +0800

    chore: 移除调试信息

--- examples/build/webpack.base.conf.js
+++ examples/build/webpack.base.conf.js
@@ -22,9 +22,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
-      'vimo': resolve('../src/index'), // for vimo module
-      'components': resolve('../src') // for vimo module
+      '@': resolve('src')
     }
   },
   module: {
