734d65444434822c3e3c85ce9263a1266cfe4737
Eric <nostaff@sina.com>
2017-11-20 02:51:24 +0800

    feat: 修改组件的注册方式，为按需加载做准备

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -3,7 +3,7 @@
 // we are also using it with karma-webpack
 //   https://github.com/webpack/karma-webpack
 
-var webpackConfig = require('./webpack.test.conf')
+var webpackConfig = require('../../examples/build/webpack.test.conf')
 
 module.exports = function (config) {
   config.set({
@@ -12,7 +12,7 @@ module.exports = function (config) {
     //    http://karma-runner.github.io/0.13/config/browsers.html
     // 2. add it to the `browsers` array below.
     browsers: ['PhantomJS'],
-    frameworks: ['mocha', 'sinon-chai'],
+    frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
     files: ['./index.js'],
     preprocessors: {
@@ -22,16 +22,11 @@ module.exports = function (config) {
     webpackMiddleware: {
       noInfo: true
     },
-    client: {
-      mocha: {
-        timeout: 50000 // set to 5000
-      }
-    },
     coverageReporter: {
       dir: './coverage',
       reporters: [
-        {type: 'lcov', subdir: '.'},
-        {type: 'text-summary'}
+        { type: 'lcov', subdir: '.' },
+        { type: 'text-summary' }
       ]
     }
   })
