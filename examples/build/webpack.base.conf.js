e73a626cfd81a19d5c63009ff9f25989a4e1f817
xiangsongtao <280304286@163.com>
2017-11-24 14:22:02 +0800

    chore: demo构建引入目录修改

--- examples/build/webpack.base.conf.js
+++ examples/build/webpack.base.conf.js
@@ -23,7 +23,7 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
-      'vimo': resolve('../lib'),
+      'vimo': resolve('../src'),
       '@pages': resolve('src/pages'),
       '@components': resolve('../src/components'),
       '@util': resolve('../src/util') // for vimo module
