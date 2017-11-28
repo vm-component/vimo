8cb2c763f437995b0d6b9d625f4fa559658e8616
xiangsongtao <280304286@163.com>
2017-11-28 20:05:53 +0800

    chore: 组件样式检查及结构调整

--- src/components/avatar/avatar.vue
+++ src/components/avatar/avatar.vue
@@ -3,4 +3,29 @@
         <slot></slot>
     </div>
 </template>
-<script type="text/javascript" src="./avatar.js"></script>
+<script type="text/javascript">
+  /**
+   * @component Avatar
+   * @description
+   *
+   * ## 列表组件 / Avatar圆形头像组件
+   *
+   * 在Item组件中放置圆形头像的组件
+   *
+   * @see component:List
+   * @see component:Item
+   * @see component:Thumbnail
+   * @demo #/avatarList
+   *
+   * @usage
+   *
+   * import Avatar from 'vimo/lib/avatar'
+   * Vue.component(Avatar.name, Avatar)
+   *
+   * <Avatar><img src=''></Avatar>
+   *
+   * */
+  export default {
+    name: 'Avatar'
+  }
+</script>
--- src/components/badge/badge.vue
+++ src/components/badge/badge.vue
@@ -3,5 +3,47 @@
         <slot></slot>
     </div>
 </template>
-<style lang="scss" src="./badge.scss"></style>
-<script type="text/javascript" src="./badge.js"></script>
+<style lang="scss" src="./style.scss"></style>
+<script type="text/javascript">
+  /**
+   * @component Badge
+   * @description
+   *
+   * ## 其他 / Badge徽章组件
+   *
+   * 徽章组件, 可在Item中组合使用.
+   *
+   * @props {String} [color] - 颜色
+   * @props {String} [mode='ios'] - 模式
+   *
+   * @slot 空 - 默认嵌入全部
+   *
+   * @demo #/badge
+   * */
+  export default {
+    name: 'Badge',
+    props: {
+      /**
+       * 按钮color：primary、secondary、danger、light、dark
+       * */
+      color: String,
+      /**
+       * mode 按钮风格 ios/window/android/we/alipay
+       * */
+      mode: {
+        type: String,
+        default () { return this.$config && this.$config.get('mode') || 'ios' }
+      }
+    },
+    computed: {
+      // 环境样式
+      modeClass () {
+        return `badge badge-${this.mode}`
+      },
+      // 颜色
+      colorClass () {
+        return this.color ? (`badge-${this.mode}-${this.color}`) : ''
+      }
+    }
+  }
+</script>
