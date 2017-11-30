0567517cf078b25f1b63d31dedda9aa3519c44d6
xiangsongtao <280304286@163.com>
2017-11-30 17:20:40 +0800

    test: fix test of InfiniteScrollContent

--- src/components/infinite-scroll-content/__tests__/index.spec.js
+++ src/components/infinite-scroll-content/__tests__/index.spec.js
@@ -13,7 +13,7 @@ let options = {
 describe('InfiniteScrollContent', function () {
   it('@base: renders the correct markup', function () {
     let wrapper = mount(InfiniteScrollContent, options)
-    const result = `<div class="ion-infinite-scroll-content"><div class="infinite-loading"><div class="infinite-loading-spinner"><div class="spinner"> </div></div> <div class="infinite-loading-text">Loading more data...</div></div></div>`
+    const result = `<div class="ion-infinite-scroll-content"><div class="infinite-loading"><div class="infinite-loading-spinner"><div class="ion-spinner"> </div></div> <div class="infinite-loading-text">Loading more data...</div></div></div>`
     expect(wrapper.html()).toEqual(result)
   })
 
